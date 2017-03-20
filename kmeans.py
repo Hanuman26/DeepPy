c8f2af0946fd8d8d0594f48f0759022bce3c2f63
Hanuman26 <manishpande1520@gmail.com>
2017-03-20 23:05:31 +0530

    benchmarking k means

--- kmeans.py
+++ kmeans.py
@@ -12,4 +12,13 @@ labels = digits.target
 
 sample_size = 300
 
-print("
+print("n_digits: %d, \t n_samples: %d, \t n_features %d" %(n_digits, n_samples,n_features))
+print(79 * '_')
+print('% 9s' % 'init''      time inertia homo compl v-meas ARI AMI silhouette')
+
+def bench_k_means(estimator, name, data):
+    t0 = time()
+    estimator.fit(data)
+    print('% 9s %.2fs %i %.3f %.3f %.3f %.3f'
+            %(name, (time() - t0), estimator.inertia_,
+                metrics.homogeneity_score(labels, estimator.labels_),
